3af8d7076c72018dcae353b60b0de87fde59eb5d
nicolas.rocq <nicolas.rocq@telecom-sudparis.eu>
2024-05-30 18:17:45 +0200

    index modification

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
         <div id="page-content-wrapper">
             <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
                 <div class="container-fluid">
-                    <button class="btn btn-primary" id="sidebarToggle"><< Menu >></button>
+                    <button class="btn btn-primary" id="sidebarToggle">Menu</button>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                     <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
                         <li class="nav-item active"><a class="nav-link" href="https://gitlabev.imtbs-tsp.eu" target="_blank">GitLabEv</a></li>
