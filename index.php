f2109374e40ba2209cb0600065be97f6a2a2f4b2
nicolas.rocq <nicolas.rocq@telecom-sudparis.eu>
2024-06-01 20:29:05 +0200

    add some margin

--- index.php
+++ index.php
@@ -55,9 +55,9 @@
                     <button class="btn btn-primary" id="sidebarToggle">Menu</button>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                     <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
-                        <li class="nav-item active"><a class="nav-link" href="https://github.com/Nishogi/EDEN" target="_blank">code source</a></li>
+                        <li class="nav-item active ms-auto mt-2 mt-lg-0"><a class="nav-link" href="https://github.com/Nishogi/EDEN" target="_blank">code source</a></li>
                         <liv class="nav-item active">
-                        <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Team</a>
+                        <a class="nav-link dropdown-toggle ms-auto mt-2 mt-lg-0" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Team</a>
                             <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="#!">franz.bertin@imtbs-tsp.eu</b></a>
                                 <a class="dropdown-item" href="#!">christophe.gaboret@imtbs-tsp.eu</b></a>
