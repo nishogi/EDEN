8bd25d08e8bbb7f75d6e9fa519beb286db82a1a5
Tom BURELLIER <tom.burellier@telecom-sudparis.eu>
2024-06-04 17:29:23 +0200

    ajout de l'entrée rm dans le script proxy, update du readme associé

--- proxy/script.sh
+++ proxy/script.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
 # Vérifier les arguments
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <client_name> <server_addr> <port>"
+if [ "$#" -ne 2 ] && [ "$#" -ne 4 ]; then
+    echo "Usage: $0 <add|rm> <client_name> [<server_addr>] [<port>]"
     exit 1
 fi
 
 # Variables
-API_ENDPOINT="http://157.159.11.199:5000/add_config"
-CLIENT_NAME="$1"
-SERVER_ADDR="$2:22"
-PORT="$3"
+USAGE="$1"
+API_ENDPOINT_ADD="http://157.159.11.199:5000/add_config"
+API_ENDPOINT_RM="http://157.159.11.199:5000/remove_config"
+CLIENT_NAME="$2"
+SERVER_ADDR="$3:22"
+PORT="$4"
 
-# Faire la requête avec curl
-curl -X POST -H "Content-Type: application/json" -d '{"client_name":"'"$CLIENT_NAME"'", "port":"'"$PORT"'", "ip":"'"$SERVER_ADDR"'"}' "$API_ENDPOINT"
+# Vérifier si c'est une opération d'ajout ou de suppression
+if [ "$USAGE" = "add" ]; then
+    # Faire la requête avec curl pour l'ajout
+    curl -X POST -H "Content-Type: application/json" -d '{"client_name":"'"$CLIENT_NAME"'", "port":"'"$PORT"'", "ip":"'"$SERVER_ADDR"'"}' "$API_ENDPOINT_ADD"
+elif [ "$USAGE" = "rm" ]; then
+    # Faire la requête avec curl pour la suppression
+    curl -X DELETE -H "Content-Type: application/json" -d '{"client_name":"'"$CLIENT_NAME"'"}' "$API_ENDPOINT_RM"
+else
+    echo "Usage: $0 <add|rm> <client_name> <server_addr> [<port>]"
+    exit 1
+fi
